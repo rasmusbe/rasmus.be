aa13552b24d276980e285743944df6f0520fa91b
DeployBot <rasmusbe@users.noreply.github.com>
2020-07-13 11:25:59 +0000

    Deploy(24eb49e): Git workflow
    Repo: rasmusbe/rasmus.be
    Workflow: .github/workflows/deploy.yml
    SHA: 24eb49e1bd7fc01766738046429e84d3edc82a59
    Ref: refs/heads/master
    Actor: rasmusbe

---
+++ _next/static/xoAAL6GQtY1PJVr1bzaa1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002Fb2e984c5.4d970977b03fd6c41e76.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
